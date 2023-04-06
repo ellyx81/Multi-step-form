113cd30fab7d04f81354a02aa5ee2ec37f98aecf
Giselle Santos <gisellesan0801@gmail.com>
2023-04-06 19:17:08 +0800

    seperated div tab and contact-form__button-container tab and created function.js with function that displays current tab and fix the previous/next buttons

---
+++ function.js
@@ -0,0 +1,21 @@
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form ...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  // ... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("contact-form__previous-button").style.display = "none";
+  } else {
+    document.getElementById("contact-form__previous-button").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("contact-form__next-button").innerHTML = "Submit";
+  } else {
+    document.getElementById("contact-form__next-button").innerHTML = "Next";
+  }
+  // ... and run a function that displays the correct step indicator:
+  fixStepIndicator(n)
+}
